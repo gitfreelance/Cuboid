7123541b0323d20eb94ffe92aac5f6e50f542024
mspoku <mp1428@aol.com>
2018-01-25 01:37:09 -0800

    Initial commit

---
+++ Source/Cuboid/CuboidGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "CuboidGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CUBOID_API ACuboidGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
